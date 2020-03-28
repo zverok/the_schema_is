4ab85df16ba93acdf01b2a82bc4ba75720d8b062
zverok <zverok.offline@gmail.com>
2020-03-28 22:30:37 +0200

    AR DSL

--- lib/the_schema_is.rb
+++ lib/the_schema_is.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
+require 'active_support'
+
 module TheSchemaIs
+  module DSL
+    # Just a no-op!
+    def the_schema_is(*); end
+  end
+end
+
+ActiveSupport.on_load(:active_record) do
+  extend TheSchemaIs::DSL
 end
